c4ec699efe2f84e9075aeb6b37cd46510cd1124a
Kim <you@example.com>
2025-08-21 23:06:49 +0800

    fix: add env

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -63,8 +63,9 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
       reload(user.id);
       StoreLoad("store.json", { autoSave: false }).then((store) => {
         store.get("menu").then((menu) => {
+          console.log("Loaded menu from store:", menu);
           const menuList = menu as StockTableType[];
-          if (menuList.length === 0) {
+          if (!menuList || menuList.length === 0) {
             console.warn("Menu is empty, please update your menu.");
             handleDownloadMenu();
           }
